9c28c4c483413260cac72b2cbb96d86631d43d57
Derek Gooding <djgooding@live.com>
2025-06-16 11:30:29 -0400

    hmm...

---
+++ SimpleMVVM.Example/Elements.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleMVVM.Example;
+
+public record struct Element(string Name, string Description) : INamed;
+[Singleton]
+public partial class Elements : IContent<Element>
+{
+    public Element[] All { get; } =
+        [
+            new Element("Element 1", "Description of Element 1"),
+            new Element("Element 2", "Description of Element 2"),
+            new Element("Element 3", "Description of Element 3"),
+            new Element("Element 4", "Description of Element 4"),
+            new Element("Element 5", "Description of Element 5")
+        ];
+}
--- SimpleMVVM.Example/MainViewModel.cs
+++ SimpleMVVM.Example/MainViewModel.cs
@@ -1,8 +1,15 @@
 ﻿namespace SimpleMVVM.Example;
 
-[Singleton, ViewModel]
+[Singleton][ViewModel]
 public partial class MainViewModel
 {
     [Command]
     public void ShowMessage() => System.Windows.MessageBox.Show("Hello, World!");
+
+    [Command]
+    public void ShowElements()
+    {
+        var elements = App.Get<Elements>().All;
+        System.Windows.MessageBox.Show(string.Join("\n", elements.Select(e => $"{e.Name}: {e.Description}")));
+    }
 }
